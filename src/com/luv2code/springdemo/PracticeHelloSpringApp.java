98e3e8a07c522b4336566b925e8fe3090e7a173f
Larissa Rabello <lari.rabello@gmail.com>
2017-04-24 15:38:54 -0300

    [Lecture 50] - practice activity #3 using prototype scope in GolfCoach

--- src/com/luv2code/springdemo/PracticeHelloSpringApp.java
+++ src/com/luv2code/springdemo/PracticeHelloSpringApp.java
@@ -17,6 +17,21 @@ public class PracticeHelloSpringApp {
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
 		
+		
+		
+		// retrieve bean from spring container	
+		Coach alphaCoach = context.getBean("myGolfCoach", Coach.class);
+		
+		// check if they are the same
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object "+ result);
+		
+		System.out.println("\nMemory location for theCoach "+ theCoach +"\n");
+		
+		System.out.println("\nMemory location for alphaCoach "+ alphaCoach +"\n");
+		
 		// close the context
 		context.close(); 
 	}
